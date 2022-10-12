a65e0571fa2f186566967be2966ec33986c5008f
Daniel Jackson <danielj683@gmail.com>
2022-10-12 11:26:11 +0100

    Add data
    Added .csv files to data folder
    Modified data.py to reflect that
    Added .gitignore

--- data.py
+++ data.py
@@ -1,17 +1,21 @@
 import torch
 from torch.utils.data import DataLoader
 import torchdata.datapipes as dp
-
+from einops import rearrange
 import numpy as np
 import os
 
+BATCH_SIZE = 8
+
 def row_processor(row):
+    data = np.array(row[1:], np.float64)
+    data = rearrange(data, '(h w) -> h w', h=28)
     return {
         'label': np.array(row[0], np.int32),
-        'data': np.array(row[1:], np.float64)
+        'data': data
     }
 
-def build_datapipes(root_dir='data', set='train'):
+def build_datapipes(set, root_dir='data'):
     data_dir = os.path.join(root_dir, set)
     data_pipe = dp.iter.FileLister(data_dir)
     data_pipe = data_pipe.open_files(mode='rt')
@@ -20,20 +24,26 @@ def build_datapipes(root_dir='data', set='train'):
     data_pipe = data_pipe.map(row_processor)
     return data_pipe
 
-if __name__ == '__main__':
-    data_pipe = build_datapipes()
-    dl = DataLoader(
-        dataset=data_pipe,
-        batch_size=8,
-        num_workers=2
-    )
+def build_dataloaders():
+    dataloaders = {}
+    for set in ['train', 'val', 'test']:
+        dataloaders[set] = DataLoader(
+            dataset=build_datapipes(set),
+            batch_size=BATCH_SIZE,
+            num_workers=2
+        )
+    return dataloaders
 
-    first = next(iter(dl))
+if __name__ == '__main__':
+
+    dataloaders = build_dataloaders()
+
+    first = next(iter(dataloaders['train']))
     labels, features = first['label'], first['data']
     print(f"Labels batch shape: {labels.size()}")
     print(f"Features batch shape: {features.size()}")
 
-    n_sample = 0
-    for row in iter(dl):
-        n_sample += 1
-    print(f'n_sample = {n_sample}')
+    # n_sample = 0
+    # for row in iter(dl):
+    #     n_sample += 1
+    # print(f'n_sample = {n_sample}')
